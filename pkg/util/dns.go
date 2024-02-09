f1be84e9fd3d3a75a63b44a0634feec225596e36
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-09 17:09:52 -0500

    add logging to resolver

--- pkg/util/dns.go
+++ pkg/util/dns.go
@@ -10,6 +10,7 @@ func ResolveHost(hostname string) ([]net.IP, error) {
 	if strings.Contains(hostname, "*") {
 		hostname = fmt.Sprintf("test%s", hostname[1:])
 	}
+	fmt.Printf("checking host %s\n", hostname)
 	ips, err := net.LookupIP(hostname)
 	if err != nil {
 		return nil, fmt.Errorf("unable to resolve %s: %v", hostname, err)
