ec084ad5e31e279020ef60971e49a721c3801683
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-09 15:52:07 -0500

    drop wildcard on lookup

--- pkg/util/dns.go
+++ pkg/util/dns.go
@@ -3,9 +3,13 @@ package util
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func ResolveHost(hostname string) ([]net.IP, error) {
+	if strings.Contains(hostname, "*") {
+		hostname = fmt.Sprintf("test%s", hostname[1:])
+	}
 	ips, err := net.LookupIP(hostname)
 	if err != nil {
 		return nil, fmt.Errorf("unable to resolve %s: %v", hostname, err)
