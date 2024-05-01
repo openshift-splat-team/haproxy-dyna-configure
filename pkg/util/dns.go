9288bb3b452cd3a2ce55490a19b968619e7af729
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-05-01 15:23:23 -0400

    only check for api-int record and derive ingress and api endpoints from that

--- pkg/util/dns.go
+++ pkg/util/dns.go
@@ -3,13 +3,9 @@ package util
 import (
 	"fmt"
 	"net"
-	"strings"
 )
 
 func ResolveHost(hostname string) ([]net.IP, error) {
-	if strings.Contains(hostname, "*") {
-		hostname = fmt.Sprintf("test%s", hostname[1:])
-	}
 	fmt.Printf("checking host %s\n", hostname)
 	ips, err := net.LookupIP(hostname)
 	if err != nil {
